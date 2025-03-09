2776158f96c15de7abe50a9256a70e743670124e
akash_appscook <akash@appscook.com>
2025-03-09 19:43:16 +0530

    Updated work status

--- work_update/serializers.py
+++ work_update/serializers.py
@@ -16,6 +16,18 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Task
         fields = '__all__'  
+    def create(self, validated_data):
+        # Handle the work_status field separately
+        work_status_id = self.initial_data.get('work_status')
+        if work_status_id:
+            try:
+                work_status = TaskState.objects.get(id=work_status_id)
+                validated_data['work_status'] = work_status
+            except TaskState.DoesNotExist:
+                raise serializers.ValidationError({"work_status": "Invalid TaskState ID"})
+
+        return super().create(validated_data)
+
     def update(self, instance, validated_data):
         # Handle the work_status field separately
         work_status_id = self.initial_data.get('work_status')
